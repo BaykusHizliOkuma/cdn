c0cb5cc586ce81119637cf4ce71ede4014daf9c0
Hüseyin <iletisim@huseyinyigit.com.tr>
2022-12-30 22:47:43 +0300

    themes added.

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+
+################################################################################
+# Add Nginx server block
+################################################################################
+rm -rf /etc/nginx/sites-enabled/cdn.baykushizliokuma.com
+nginx -s reload
+nginx -t
+cat > /etc/nginx/sites-enabled/cdn.baykushizliokuma.com <<- EOM
+server{
+  listen 80;
+  listen [::]:80;
+
+  root /root/BaykusHizliOkuma/cdn;
+
+  index index.html index.htm index.nginx-debian.html;
+  
+  server_name cdn.baykushizliokuma.com;
+
+  return 404;
+}
+EOM
+nginx -t
+nginx -s reload
+
+gpasswd -a www-data $USER
+chmod g+x $HOME/
+chmod g+x $HOME/BaykusHizliOkuma/
+chmod g+x $HOME/BaykusHizliOkuma/cdn/
+nginx -s reload
+
+sudo certbot --nginx -d cdn.baykushizliokuma.com
+nginx -s reload
+
+
--- themes/startbootstrap-resume/nginx/allow_access.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-gpasswd -a www-data root
-chmod g+x /root
-chmod g+x /root/sites
-chmod g+x /root/sites/huseyinyigit.com.tr
-chmod g+x /root/html
-chmod g+x /root/html/themes
-chmod g+x /root/html/themes/startbootstrap-resume
-nginx -s reload
